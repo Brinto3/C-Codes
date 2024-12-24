02c6c1ad0475237ad3a1351771627e5895d7b0ff
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-24 22:17:39 +0600

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+cout<<"Hello world"; //To print manually.
+
+string Word;
+cout<<"Enter a string you want to display:";
+cin>>Word;
+cout<<Word<<endl;    //To print user defined words.
+
+return 0;
+}
