4fd1c93d9c55a2bb72c11c441e990cb6b7573a75
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-24 22:18:06 +0600

    Add files via upload

---
+++ All_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int num1 = 19;
+double num2 = 20.361;
+float num3 = 55.3698;
+char ch = 'a';
+
+cout<< num1 <<endl;
+cout<< num2 <<endl;
+cout<< num3 <<endl;
+cout<< ch <<endl;
+
+return 0;
+}
