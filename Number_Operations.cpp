2defad126a325c58f6704f295763e3194c488c4a
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-25 10:55:58 +0600

    Add files via upload

---
+++ Number_Operations.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int num1 = 21;
+double num2 = 13.52;
+float num3 = 3.1568;
+
+cout<<"Doing Addition :"<< num1 + num2 <<endl;
+cout<<"Doing Substraction :"<< num2 - num3 <<endl;
+cout<<"Doing Multiplication :"<< num2 * num3 <<endl;
+cout<<"Doing Division :"<< num2 / num1 <<endl;
+
+return 0;
+}
