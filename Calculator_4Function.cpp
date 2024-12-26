0d5fc28279989bb12ffb00edc499fd2b2f0b01cc
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-26 12:36:12 +0600

    Update Calculator_4Function.cpp

--- Calculator_4Function.cpp
+++ Calculator_4Function.cpp
@@ -3,6 +3,35 @@ using namespace std;
 int main(){
 
 float num1, num2;
+int op;
 
+cout<<"Enter number 1 : "<<endl;
+cin>>num1;
+cout<<"Enter number 2 : "<<endl;
+cin>>num2;
 
+cout<<"Option 1 for addition"<<endl;
+cout<<"Option 2 for substraction"<<endl;
+cout<<"Option 3 for multiplication"<<endl;
+cout<<"Option 4 for division"<<endl;
+
+cout<<"Choose an operation: "<<endl;
+cin>>op;
+if (op == 1) {
+  cout<< num1 + num2 << endl;
+  } else if (op == 2) {
+    cout << num1 - num2 << endl;
+ } else if (op == 3) {
+ cout << num1 * num2 << endl;
+   } else if (op == 4) {
+   if (num2 != 0) {
+     cout << num1 / num2 << endl;
+   } else {
+       cout << "Error zero not allowed" << endl;
+    }
+  } else {
+ cout << "Invalid"<< endl;
+  }
+
+return 0;
 }
