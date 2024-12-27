17866dde83b23f6ce0f42309ce2133022af5d2cd
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-27 19:16:55 +0600

    Add files via upload

---
+++ Triangle_Area.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+float base, height, area;
+
+cout<<"Enter the base:";
+cin>>base;
+
+cout<<"Enter the height:";
+cin>>height;
+
+area = 0.5 * base * height;
+
+cout<<"The area of triangle is:"<<area;
+
+return 0;
+}
