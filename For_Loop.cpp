d845c93e8fe1320b6dc5a7c718189b3e6c73a3ae
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2024-12-30 20:40:30 +0600

    Add files via upload

---
+++ For_Loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+string name;
+cout<<"Enter a name:"<<endl;
+cin>>name;
+
+for(int i=0; i<=3; i++){
+
+    cout<<name<<" "<<i<<endl;
+}
+
+return 0;
+}
